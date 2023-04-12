4506fc970a8e28e788036e1cebe612c1257bb50d
osfa <jacob.broms@prototyp.se>
2023-04-12 15:40:15 +0200

    shrug

--- src/components/ScrapBook.vue
+++ src/components/ScrapBook.vue
@@ -3,6 +3,28 @@
     <v-stage ref="stage" :config="configKonva" @dragstart="handleDragstart" @dragend="handleDragend">
       <v-layer ref="layer">
         <v-image
+          v-for="item in list"
+          :key="item.id"
+          :config="{
+            x: item.x,
+            y: item.y,
+            rotation: item.rotation,
+
+            id: item.id,
+            image: item.image,
+            draggable: true,
+            width: 500,
+            height: 353,
+
+            // scaleX: dragItemId === item.id ? item.scale * 1.2 : item.scale,
+            // scaleY: dragItemId === item.id ? item.scale * 1.2 : item.scale,
+            // shadowColor: 'black',
+            // shadowBlur: 10,
+            // shadowOffsetX: dragItemId === item.id ? 15 : 5,
+            // shadowOffsetY: dragItemId === item.id ? 15 : 5,
+            // shadowOpacity: 0.6,
+          }"></v-image>
+        <!-- <v-image
           :config="{
             image: image,
             width: 500,
@@ -22,7 +44,7 @@
             stroke: 'white',
             strokeWidth: 10,
             rotation: 5,
-          }" />
+          }" /> -->
         <!-- <v-star
           v-for="item in list"
           :key="item.id"
@@ -53,9 +75,12 @@
 <script>
 const width = window.innerWidth;
 const height = window.innerHeight;
+import { imgLibrary } from "../imgLibrary.js";
+
 export default {
   data() {
     return {
+      imgPaths: imgLibrary.testBatch,
       list: [],
       dragItemId: null,
       configKonva: {
@@ -80,33 +105,55 @@ export default {
     handleDragend(e) {
       this.dragItemId = null;
     },
+    randomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
+    },
+    randomFloat(min, max) {
+      const r = Math.random() * (max - min) + min;
+      return r;
+    },
   },
   mounted() {
-    for (let n = 0; n < 30; n++) {
+    this.imgPaths.forEach((element) => {
+      console.log(element);
+      const image = new window.Image();
+      image.src = element;
       this.list.push({
+        image,
         id: Math.round(Math.random() * 10000).toString(),
         x: Math.random() * width,
         y: Math.random() * height,
         rotation: Math.random() * 180,
-        scale: Math.random(),
+        // scale: Math.random(),
       });
-    }
+    });
+    // for (let n = 0; n < 30; n++) {
+    //   this.list.push({
+    //     id: Math.round(Math.random() * 10000).toString(),
+    //     x: Math.random() * width,
+    //     y: Math.random() * height,
+    //     rotation: Math.random() * 180,
+    //     scale: Math.random(),
+    //   });
+    // }
   },
   created() {
-    const image = new window.Image();
-    image.src = "./00001-2017427526_46.jpg";
-    image.onload = () => {
-      // set image only when it is loaded
-      console.log("loaded");
-      this.image = image;
-    };
-    const lfsImage = new window.Image();
-    lfsImage.src = "./memories/00007-2017427528_44.png";
-    lfsImage.onload = () => {
-      // set image only when it is loaded
-      console.log("loaded");
-      this.lfsImage = lfsImage;
-    };
+    // const image = new window.Image();
+    // image.src = "./00001-2017427526_46.jpg";
+    // image.onload = () => {
+    //   // set image only when it is loaded
+    //   console.log("loaded");
+    //   this.image = image;
+    // };
+    // const lfsImage = new window.Image();
+    // lfsImage.src = "./memories/00007-2017427528_44.png";
+    // lfsImage.onload = () => {
+    //   // set image only when it is loaded
+    //   console.log("loaded");
+    //   this.lfsImage = lfsImage;
+    // };
   },
 };
 </script>
---
+++ src/imgLibrary.js
@@ -0,0 +1,3 @@
+export const imgLibrary = {
+  testBatch: ["./memories/00007-2017427528_44.png", "./memories/00014-956240287_23.png", "./memories/00015-2017427528_13.png"],
+};
