e3c35b3b4a1be142e5317cda2d6f7af596fabc83
osfa <jacob.broms@prototyp.se>
2023-05-04 18:15:47 +0200

    controls crude test

--- src/components/AudioVisualBook.vue
+++ src/components/AudioVisualBook.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <!-- <div class="controls"><input v-model="tickInterval" /></div> -->
-    <!-- <div class="slide-background fadeIn" ></div> -->
-    <!-- <img class="fade-in-image chapter" v-for="(chapter, idx) in chapters" :key="idx" :src="chapter.imgPath" /> -->
+    <div class="controls">
+      <div>tickInterval <input v-model="tickInterval" /></div>
+      <div>slideTickInterval <input v-model="slideTickInterval" /></div>
+      <div @click="newSequence">new sequence</div>
+      <div @click="newCard">new card</div>
+    </div>
     <div class="fade-in-image chapter-bkg" v-for="(chapter, idx) in chapters" :key="idx" :style="{ backgroundImage: 'url(' + chapter.imgPath + ')' }" />
+    <div class="sequence-container"><img class="fade-in-image chapter-card" v-for="(chapter, idx) in chapters.reverse()" :key="idx" :src="chapter.imgPath" /></div>
+    <img class="slot" @click="newSequence" v-for="(slot, idx) in slots" :key="idx" :src="slot.imgPath" />
   </div>
 </template>
 
 <script>
 import { imgLibrary } from "../imgLibrary.js";
 // const basePath = "./memories/batch-1-500k/";
-const basePath = "./memories/batch-2-depth-500k/";
+// const basePath = "./memories/batch-2-depth-500k/";
 
+const anime911 = imgLibrary.anime911.map((x) => "./memories/batch-911-anime-sel1-500k/" + x);
+const secondelife911 = imgLibrary.secondlife911.map((x) => "./memories/batch-911-second-life-sel1-500k/" + x);
+const batch1 = imgLibrary.batch1.map((x) => "./memories/batch-1-500k/" + x);
+const batch2 = imgLibrary.batch2.map((x) => "./memories/batch-2-depth-500k/" + x);
 export default {
   data() {
     return {
@@ -20,18 +29,31 @@ export default {
       lastNow: null,
       ticks: 0,
       tickInterval: 1000,
-      slideTickInterval: 10,
+      slideTickInterval: 4,
       imgIdx: 0,
 
       chapters: [],
-      // batch: imgLibrary.batch2,
-      batch: imgLibrary.batch2.map((x) => "./memories/batch-2-depth-500k/" + x).concat(imgLibrary.batch1.map((x) => "./memories/batch-1-500k/" + x)),
+      batch: batch2.concat(batch1).concat(anime911).concat(secondelife911),
+
+      currentSequence: [],
+      sequences: [],
+      slots: [],
     };
   },
   methods: {
+    newCard() {
+      const chapter = {
+        imgPath: this.batch.sample(),
+        audioClip: null,
+      };
+      this.currentSequence.push(chapter);
+    },
+    newSequence() {
+      this.sequences.push(this.currentSequence);
+      this.currentSequence = [];
+    },
     tick() {
       this.ticks += 1;
-      // this.addMemory(this.imgPaths.sample());
       // console.log(this.$refs.layer);
       if (this.ticks % this.slideTickInterval === 0) {
         // this.$emit('tick16')
@@ -40,7 +62,6 @@ export default {
         image.onload = () => {
           console.log("loaded");
           this.imgIdx += 1;
-          // this.createChapter(this.imgPaths[this.imgIdx]);
           const imgPath = this.batch.sample();
           this.createChapter(imgPath);
         };
@@ -74,10 +95,6 @@ export default {
     },
   },
   mounted() {
-    // this.imgPaths.forEach((element) => {
-    //   this.addMemory(element);
-    // });
-    // this.createChapter(this.imgPaths[this.imgIdx]);
     const imgPath = this.batch.sample();
     this.createChapter(imgPath);
     this.frame();
@@ -132,16 +149,42 @@ export default {
   animation-iteration-count: infinite;
   animation-direction: alternate;
 }
+html,
 body {
   margin: 0;
   padding: 0;
 }
+
 .controls {
   position: fixed;
-  top: 50px;
-  right: 0px;
-  z-index: 1000;
+  top: 25px;
+  left: 25px;
+  z-index: 3000;
 }
+
+.sequence-container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  overflow-x: hidden;
+  justify-content: center;
+}
+.chapter-card {
+  width: 30vw;
+  z-index: 1000;
+  margin: 0;
+  padding: 0;
+  margin: 10px;
+}
+.chapter-card {
+  width: 48vw;
+  z-index: 2000;
+  margin: 0;
+  padding: 0;
+}
+
 .chapter {
   width: 100vw;
   height: 100vh;
@@ -158,6 +201,7 @@ body {
   top: 0;
   left: 0;
 }
+
 @media (orientation: landscape) {
   .chapter-bkg {
     background-size: cover;
